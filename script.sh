f5979f1f25ce77671e3435ac84c32015edbc9487
Thiago Thomaz <thiagorthomaz@gmail.com>
2017-09-02 12:29:17 -0300

    Improved script

--- script.sh
+++ script.sh
@@ -5,18 +5,19 @@
 #Data:		26/09/2017		|
 #Requirement:	GNU-bash		|
 #Version	0.1			|
+#Use:		Used tp configure your	|
+#		linux environment	|
 #---------------------------------------+
 
 #========== </variables> ================
-
+version="0.1"
 PROGRAM_LIST=( "apache2" "php" "mysql" )
 APACHE_MODULES=( "mod_rewrite" )
 PHP_MODULES=( "php_gd" "php_mysql" "php_pdo" "php_cli" )
+command_list=$*
 
 #========== </Functions> ================
-
-function _MENU() {
-
+_MENU() {
 	clear
 
 	echo "============================================="
@@ -67,9 +68,38 @@ function _MENU() {
 		1 ) _INSTALL;;
 		2 ) echo "They wont be installed";;
 		3 ) _EXIT;;
-		* ) _MENU;;
+		* ) error;;
 esac
 
+}
+
+
+function _command() {
+
+	while [ "$command_list" ] 
+	do
+	        case $command_list in
+        	        -h|--help) echo "Sorry I didn't write the help yet."; exit 0;;
+	                -v|--version) shift; _version; exit 0;;
+        	        *) error;;
+	        esac
+        	shift
+	done
+
+}
+
+function _version() {
+	echo ""
+	echo "Environment: $version"
+	echo "(c) 2017"
+	echo ""
+}
+
+function error() {
+	echo ""
+	echo "Ops. Command not found."
+	echo ""
+	exit 1;
 }
 
 function _BACK() {
@@ -79,11 +109,11 @@ function _BACK() {
         echo "1 - Yes"
         echo "2 - Exit"
 
-        read -n1 op
+        read op
         case $op in
                 1 ) _MENU;;
                 2 ) _EXIT;;
-                * ) _MENU;;
+                * ) error;;
 	esac
 
 
@@ -93,17 +123,23 @@ function _INSTALL() {
 	echo "---------------------------"
 	echo "Installing apache2"
 	echo "---------------------------"
+	echo ""
 	sudo apt-get install apache2 apache2-utils
+	echo ""
 
         echo "---------------------------"
         echo "Installing php"
         echo "---------------------------"
+	echo ""
         sudo apt-get install php php-cli php-gd php-mysql
+	echo ""
 
         echo "---------------------------"
         echo "Installing libapache2-mod-php"
         echo "---------------------------"
+	echo ""
         sudo apt-get install libapache2-mod-php
+	echo ""
 
 
 	echo ""
@@ -132,7 +168,8 @@ function _EXIT() {
 
 #========== </Begin> ====================
 
-_MENU
+[ "$1" ] && _command || _MENU;
+#_MENU
 
 
 
